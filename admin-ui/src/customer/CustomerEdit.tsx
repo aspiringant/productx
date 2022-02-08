361e518fd8bf86471d9da98fc3efa892f24b1fa7
Neeraj Bagdia <neeraj.bagdia@gmail.com>
2022-02-08 17:54:29 +0530

    amplication-build-ckze3hrvh100994601s6h064uedp

---
+++ admin-ui/src/customer/CustomerEdit.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import { Edit, SimpleForm, EditProps, TextInput } from "react-admin";
+
+export const CustomerEdit = (props: EditProps): React.ReactElement => {
+  return (
+    <Edit {...props}>
+      <SimpleForm>
+        <TextInput label="Email" source="email" type="email" />
+        <TextInput label="First Name" source="firstName" />
+        <TextInput label="Last Name" source="lastName" />
+        <TextInput label="Phone" source="phone" />
+      </SimpleForm>
+    </Edit>
+  );
+};
